be01afc4b00d8081ae27973365487589857465d1
pherapont <pherapont@yandex.ru>
2021-01-13 14:45:23 +0300

    module and testing

--- solution.js
+++ solution.js
@@ -50,7 +50,7 @@ const preparing = pieces => {
    return nodes  => findPiece(data, nodes);
    };
 
-function solvePuzzle(pieces) {
+const solvePuzzle = pieces => {
   const res = [pieces[0].id];
   let nodes = {
       right: pieces[0].edges.right.edgeTypeId,
@@ -66,104 +66,8 @@ function solvePuzzle(pieces) {
   return res;
 }
 
-const data = [
-  {
-    id: 1,
-    edges: {
-      top: null,
-      right: { edgeTypeId: 7, type: "outside" },
-      bottom: { edgeTypeId: 5, type: "inside" },
-      left: null,
-    },
-  },
-  {
-    id: 9,
-    edges: {
-      top: { edgeTypeId: 8, type: "inside" },
-      right: { edgeTypeId: 15, type: "inside" },
-      bottom: null,
-      left: { edgeTypeId: 5, type: "outside" },
-    },
-  },
-  {
-    id: 5,
-    edges: {
-      top: null,
-      right: { edgeTypeId: 2, type: "inside" },
-      bottom: { edgeTypeId: 1, type: "inside" },
-      left: null,
-    },
-  },
-  {
-    id: 4,
-    edges: {
-      top: { edgeTypeId: 34, type: "inside" },
-      right: { edgeTypeId: 11, type: "outside" },
-      bottom: { edgeTypeId: 7, type: "inside" },
-      left: null,
-    },
-  },
-  {
-    id: 3,
-    edges: {
-      top: { edgeTypeId: 2, type: "outside" },
-      right: null,
-      bottom: { edgeTypeId: 4, type: "outside" },
-      left: { edgeTypeId: 6, type: "inside" },
-    },
-  },
-  {
-    id: 2,
-    edges: {
-      top: { edgeTypeId: 3, type: "outside" },
-      right: { edgeTypeId: 34, type: "outside" },
-      bottom: null,
-      left: null,
-    },
-  },
-  {
-    id: 8,
-    edges: {
-      top: null,
-      right: { edgeTypeId: 15, type: "outside" },
-      bottom: { edgeTypeId: 4, type: "inside" },
-      left: null,
-    },
-  },
-  {
-    id: 7,
-    edges: {
-      top: { edgeTypeId: 3, type: "inside" },
-      right: null,
-      bottom: { edgeTypeId: 1, type: "outside" },
-      left: { edgeTypeId: 10, type: "inside" },
-    },
-  },
-  {
-    id: 6,
-    edges: {
-      top: { edgeTypeId: 11, type: "inside" },
-      right: { edgeTypeId: 10, type: "outside" },
-      bottom: { edgeTypeId: 6, type: "outside" },
-      left: { edgeTypeId: 8, type: "outside" },
-    },
-  },
-];
+export {solvePuzzle, handler};
 
-solvePuzzle(data);
-
-const res_1 = handler(
-  {
-    id: 6,
-    edges: {
-      top: { edgeTypeId: 11, type: "inside" },
-      right: { edgeTypeId: 10, type: "outside" },
-      bottom: { edgeTypeId: 6, type: "outside" },
-      left: null,
-      },
-    });
-console.log({res_1});
- 
 // Не удаляйте эту строку
 // window.solvePuzzle = solvePuzzle;
 
